5ed7ea1690dd203f1f479963eeef205a6fa4ead8
LOOP <cjhm000115@gmail.com>
2022-10-23 18:30:19 +1100

    Clean

--- test.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import numpy as np
-np.set_printoptions(threshold=1000)
-groundTruth = cv2.imread('./dataset/2018-07-09-16-11-56_2018-07-09-16-11-56-702-disparity.png')
-print(groundTruth[:, :, 0])
